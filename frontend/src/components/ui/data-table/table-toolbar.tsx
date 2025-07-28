f2933877c9bdf6a178d3fefc1134b2a437613d7f
DaPanda <156377255+DaPandamonium@users.noreply.github.com>
2025-07-29 00:28:20 +0300

    Improve dialog layout for mobile browsers (#12)
    * fix(dialog): improve layout on mobile (#5)
    
    * Some tweaks to deal with issues found in review.
    
    * Remove unused div.
    
    Fixes #5.
    
    ---------
    
    Co-authored-by: wizjany <wizjany@gmail.com>

--- frontend/src/components/ui/data-table/table-toolbar.tsx
+++ frontend/src/components/ui/data-table/table-toolbar.tsx
@@ -1,13 +1,13 @@
-import type {Table} from "@tanstack/solid-table"
+import type { Table } from "@tanstack/solid-table"
 
-import {TbX as IconX} from "solid-icons/tb"
-import {Button} from "~/components/ui/button"
-import {TextField, TextFieldInput} from "~/components/ui/text-field"
+import { TbX as IconX } from "solid-icons/tb"
+import { Button } from "~/components/ui/button"
+import { TextField, TextFieldInput } from "~/components/ui/text-field"
 
-import {TableFacetedFilter, TableFacetedFilterProps} from "./table-faceted-filter"
-import {TableViewOptions} from "./table-view-options"
-import {For, Show} from "solid-js";
-import {ensurePagesVisible} from "~/lib/utils";
+import { TableFacetedFilter, TableFacetedFilterProps } from "./table-faceted-filter"
+import { TableViewOptions } from "./table-view-options"
+import { For, Show } from "solid-js";
+import { ensurePagesVisible } from "~/lib/utils";
 
 type DataTableToolbarProps<TData> = {
     table: Table<TData>
@@ -21,7 +21,7 @@ export function TableToolbar<TData>(props: DataTableToolbarProps<TData>) {
     return (
         <div class="flex flex-col gap-2">
             <div class="flex items-center justify-between">
-                <div class="flex flex-1 items-center space-x-2">
+                <div class="flex space-x-2">
                     <Show when={props.searchColumn}>
                         <TextField
                             value={(props.table.getColumn(props.searchColumn!)?.getFilterValue() as string) ?? ""}
@@ -29,28 +29,29 @@ export function TableToolbar<TData>(props: DataTableToolbarProps<TData>) {
                                 props.table.getColumn(props.searchColumn!)?.setFilterValue(value);
                                 ensurePagesVisible(props.table);
                             }}
+                            class="w-auto lg:w-[250px]"
                         >
-                            <TextFieldInput placeholder="Search..." class="h-8 w-[150px] lg:w-[250px]"/>
+                            <TextFieldInput placeholder="Search..." class="h-8" />
                         </TextField>
                     </Show>
                     {isFiltered() && (
                         <Button
-                            variant="ghost"
+                            variant="outline"
                             onClick={() => props.table.resetColumnFilters()}
-                            class="h-8 px-2 lg:px-3"
+                            class="h-8 w-auto px-2 sm:px-3"
                         >
                             Reset
-                            <IconX/>
+                            <IconX />
                         </Button>
                     )}
                 </div>
-                <TableViewOptions table={props.table}/>
+                <TableViewOptions table={props.table} />
             </div>
-            <div class="flex items-center justify-start gap-2">
+            <div class="grid grid-cols-3 sm:flex flex-wrap items-center gap-2 w-full">
                 <For each={props.filters}>
                     {(filter) => <TableFacetedFilter {...filter} />}
                 </For>
             </div>
         </div>
-    )
+    );
 }
